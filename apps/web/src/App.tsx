4775bf955ff7385824e5c6bd9a3e47e0ecbbaa05
Rohit Khanduri <rohit.khanduri@hotmail.com>
2026-09-01 22:49:19 +0200

    chore: removed ununsed import

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -18,7 +18,6 @@ import {
   DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Separator } from "@/components/ui/separator"
